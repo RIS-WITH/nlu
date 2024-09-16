83d85458101c05d97ad3f9fb35dfeae35f0f4884
Adrien Vigne <adrien.vigne@laas.fr>
2024-09-16 14:16:32 +0200

    [node] remove swap onto + fix attributs

--- scripts/node_nlu.py
+++ scripts/node_nlu.py
@@ -22,7 +22,7 @@ class node_nlu():
         print(result)
         res = NLUParsingServiceResponse()
         res.intent = result.intent
-        res.score = result.intent_proba
+        res.comprehension_score = result.intent_proba
         res.slots = []
         for slot in result.slots:
             S = Slot()
@@ -30,49 +30,6 @@ class node_nlu():
             S.value = slot.value
             res.slots.append(S)
         return res
-
-    # def swapOntoValues(self,old_value,new_value,language):
-    #     print("change :",old_value," to : ",new_value)
-    #     concept = self._onto.classes.find(old_value)
-    #     type_concept = "classes"
-    #     if(len(concept) == 0):
-    #         print("search in individuals")
-    #         concept = self._onto.individuals.find(old_value)
-    #         type_concept = "individual"
-
-    #     print(concept)
-    #     if(len(concept)>=1):
-    #         concept = concept[0]
-    #         print("concept found : ",concept)
-    #         if type_concept == "classes":
-    #             old_names = self._onto.classes.getNames(concept)
-    #         else:
-    #             old_names = self._onto.individuals.getNames(concept)
-    #         print("old_names : ",old_names)
-    #         self._onto.feeder.waitConnected()
-    #         for old_name in old_names:
-    #             print("remove old name : ",old_name)
-    #             self._onto.feeder.removeLanguage(concept,language,old_name)
-    #         self.current_changes[concept] = {'old_values' : old_names ,'new_value' : new_value,'language':language}
-    #         print(self.current_changes)
-    #         self._onto.feeder.addLanguage(concept,language,new_value)
-    #         self._onto.feeder.waitUpdate()
-    #         print("end swap value")
-    #     else:
-    #         print("No concept found")
-
-    # def resetOnto(self,data):
-    #     print("Reset Onto to previous state")
-    #     self._onto.feeder.waitConnected()
-    #     for key,value in self.current_changes.items():
-    #         self._onto.feeder.removeLanguage(key,value["language"],value["new_value"])
-    #         for new in value["old_values"]:
-    #             print("add : ",new)
-    #             self._onto.feeder.addLanguage(key,value["language"],new)
-    #     self._onto.feeder.waitUpdate()
-    #     print("end reset onto")
-    #     self.current_changes = {}
-    #     # return EmptyResponse()
 
     def run(self):
         rospy.spin()
